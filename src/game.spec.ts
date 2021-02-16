d6411866417fb5a5fb3e64b4d38ab076c52190fa
brisberg <brandon.risberg@gmail.com>
2021-02-15 22:18:51 -0800

    Added some TODO tests for planets and game

---
+++ src/game.spec.ts
@@ -0,0 +1,19 @@
+describe('Game', () => {
+  it.todo('should move the fleet when commanded');
+
+  it.todo('should attack a planet');
+
+  it.todo('should gather resources');
+
+  it.todo('should fill transports');
+
+  it.todo('should unload transports');
+
+  it.todo('should wait in statis');
+
+  it.todo('should re-allocate a planet');
+
+  it.todo('should build ships');
+
+  it.todo('should send spy satelites');
+});
---
+++ src/models/planet.spec.ts
@@ -0,0 +1,21 @@
+describe('Planet', () => {
+  it.todo('should update industry allocation');
+
+  it.todo('should reject industry allocation that does not sum to 100');
+
+  describe('Update', () => {
+    it.todo('should grow population when updated');
+
+    it.todo('should produced resources when updated');
+
+    it.todo('should replace fleet when updated and hostile');
+  });
+
+  it.todo('should lose population when attacked');
+
+  it.todo('should become occupied when attacked successfully');
+
+  it.todo('should fight off occupying fleet when occupied and updated');
+
+  it.todo('should join the empire when occupied long enough');
+});
