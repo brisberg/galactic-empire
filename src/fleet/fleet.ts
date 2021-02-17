ce5b10450068879d510d4080f24654e525240b48
brisberg <brandon.risberg@gmail.com>
2021-02-17 06:59:17 -0800

    Fixes typo in fleet file comment

--- src/fleet/fleet.ts
+++ src/fleet/fleet.ts
@@ -2,7 +2,7 @@
  * @fileoverview
  * Fleet class represents a grouping of Ships at a particular location. The
  * player controls a fleet which moves from planet to planet. Most planets also
- * posess a defence fleet. Fleets can fight each other for control of a system.
+ * possess a defence fleet. Fleets can fight each other for control of a system.
  * */
 export class Fleet {
   private ships: Map<string, number> = new Map();
