a02dc95ce621867cc6872aeecb05c6e74cf7be55
brisberg <brandon.risberg@gmail.com>
2021-02-21 20:22:52 -0800

    Adds a simple FleetStatus component

--- src/viewer/src/App.tsx
+++ src/viewer/src/App.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import './App.css';
+import FleetStatus, { Fleet } from './fleet-status/FleetStatus';
 import MapGrid from './map-grid/MapGrid';
 
 /** Main App Component */
 export default function App() {
+  const fleet: Fleet = {
+    credits: 10000, supply: 2983, supplyMax: 3000, fuel: 1732, fuelMax: 2000,
+    fighters: 160, transports: 100, mTransports: 50,
+  }
+
   const planets = [
-    {x: 3, y: 5},{x: 11, y: 2},{x: 24, y: 8},{x: 29, y: 24},{x: 7, y: 15},
-    {x: 11, y: 21},{x: 18, y: 13},{x: 3, y: 21},{x: 6, y: 28},{x: 9, y: 18}
+    { x: 3, y: 5 }, { x: 11, y: 2 }, { x: 24, y: 8 }, { x: 29, y: 24 }, { x: 7, y: 15 },
+    { x: 11, y: 21 }, { x: 18, y: 13 }, { x: 3, y: 21 }, { x: 6, y: 28 }, { x: 9, y: 18 },
   ];
 
   return (
     <div className="App">
-      <MapGrid planets={planets}/>
+      <MapGrid planets={planets} />
+      <FleetStatus fleet={fleet} />
     </div>
   );
 }
