fc4b4a8f3395490d2112baeab1abeea93e65a46d
brisberg <brandon.risberg@gmail.com>
2021-02-25 18:09:49 -0800

    ActionsPanel will catch errors generated by controller commands

--- viewer/src/actions-panel/ActionsPanel.tsx
+++ viewer/src/actions-panel/ActionsPanel.tsx
@@ -14,12 +14,21 @@ export default function ActionsPanel({ fleet, planet, controller, onUpdate }: { 
   const maxSupplyPurchase = Math.min(100, planet.getResource(Resource.SUPPLY), fleet.getCapacity(Resource.SUPPLY) - fleet.getResource(Resource.SUPPLY));
   const maxFuelPurchase = Math.min(100, planet.getResource(Resource.FUEL), fleet.getCapacity(Resource.FUEL) - fleet.getResource(Resource.FUEL));
 
+  const execute = (command: Function): void => {
+    try {
+      command();
+    } catch (err) {
+      console.log(err.toString())
+    }
+    onUpdate();
+  }
+
   return (
     <div className="list">
-      <button onClick={() => { controller.embark(planet); onUpdate(); }} disabled={embarkDisabled}>Embark</button>
-      <button onClick={() => { controller.collectTaxes(); onUpdate(); }} disabled={taxesDisabled}>Collect Taxes</button>
-      <button onClick={() => { controller.purchaseResource(Resource.SUPPLY, maxSupplyPurchase); onUpdate(); }} disabled={suppliesDisabled}>Buy Supplies (100)</button>
-      <button onClick={() => { controller.purchaseResource(Resource.FUEL, maxFuelPurchase); onUpdate(); }} disabled={fuelDisabled}>Buy Fuel (100)</button>
+      <button onClick={() => { execute(() => controller.embark(planet)) }} disabled={embarkDisabled}>Embark</button>
+      <button onClick={() => { execute(() => controller.collectTaxes()) }} disabled={taxesDisabled}>Collect Taxes</button>
+      <button onClick={() => { execute(() => controller.purchaseResource(Resource.SUPPLY, maxSupplyPurchase)) }} disabled={suppliesDisabled}>Buy Supplies (100)</button>
+      <button onClick={() => { execute(() => controller.purchaseResource(Resource.FUEL, maxFuelPurchase)) }} disabled={fuelDisabled}>Buy Fuel (100)</button>
     </div>
   );
 }
